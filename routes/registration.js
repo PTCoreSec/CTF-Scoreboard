2a70acba5fa66d9cd8e0f836e5a9fd0258f4faf5
Luke Snyder <luke@ubuntu.(none)>
2013-10-08 10:03:20 -0700

    Registration will now check for empty username and password

--- routes/registration.js
+++ routes/registration.js
@@ -11,23 +11,32 @@ exports.checkRegistration = function(req, res) {
 	var salt = randomstring.generate(20);
 	var password = hash.sha512(teamPassword, salt);
 
-	connections.connection.query('SELECT * FROM teams WHERE ?', {name: teamName}, function(err, result) {
-		console.log(result);
-		if (result[0]) res.render('session/register');
-		else {
-			connections.connection.query('INSERT INTO teams SET ?', {name: teamName, description: req.body.description, password: password, administrationLevel: 0}, function(err, result) {
-				if (err) console.log(err);
-				connections.connectionHashes.query('INSERT INTO userHashes SET ?', {idteams: result.insertId, salt: salt}, function(err, result2){
-					if(err) console.log(err);
-					res.render('session/register');
+	req.session.e = false;
+
+	if((teamPassword == "") || (teamName == "")) {
+		req.session.e = true;
+		res.render("session/register", {error: "Invalid Username/Password", title: "CyberCTF Registration"})
+	}
+
+	if((teamName != "") && (teamPassword != "" )) {
+		connections.connection.query('SELECT * FROM teams WHERE ?', {name: teamName}, function(err, result) {
+			console.log(result);
+			if (result[0]) res.render('session/register', {title: "CyberCTF Registration 1"});
+			else {
+				connections.connection.query('INSERT INTO teams SET ?', {name: teamName, description: req.body.description, password: password, administrationLevel: 0}, function(err, result) {
+					if (err) console.log(err);
+					connections.connectionHashes.query('INSERT INTO userHashes SET ?', {idteams: result.insertId, salt: salt}, function(err, result2){
+						if(err) console.log(err);
+						res.render('session/register');
+					});
 				});
-			});
-			res.redirect("/login");
-		}
-	});
+				res.redirect("/login");
+			}
+		});
+	}
 }
 
 exports.register = function(req, res) {
-	req.session.path = req.route.path;
-	res.render('session/register', {title: 'CyberCTF Register' });
+	req.session.e = false;
+	res.render('session/register', {title: 'CyberCTF Registration', error: "" });
 }
